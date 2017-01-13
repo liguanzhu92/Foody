221f94379ce671b0224b82fbfea13f30876129c5
Guanzhu Li <liguanzhu390@gmail.com>
2017-01-13 15:25:52 -0600

    add volley and volly manipulation

---
+++ app/src/main/java/com/example/guanzhuli/foody/model/VolleyController.java
@@ -0,0 +1,66 @@
+package com.example.guanzhuli.foody.model;
+
+import android.app.Application;
+import android.text.TextUtils;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.Volley;
+
+/**
+ * Created by Guanzhu Li on 1/13/2017.
+ */
+public class VolleyController extends Application {
+
+    public static final String TAG = VolleyController.class
+            .getSimpleName();
+
+    private RequestQueue mRequestQueue;
+    private ImageLoader mImageLoader;
+
+    private static VolleyController mInstance;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mInstance = this;
+    }
+
+    public static synchronized VolleyController getInstance() {
+        return mInstance;
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (mRequestQueue == null) {
+            mRequestQueue = Volley.newRequestQueue(getApplicationContext());
+        }
+
+        return mRequestQueue;
+    }
+
+    public ImageLoader getImageLoader() {
+        getRequestQueue();
+        if (mImageLoader == null) {
+            mImageLoader = new ImageLoader(this.mRequestQueue,
+                    new LruBitmapCache());
+        }
+        return this.mImageLoader;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req, String tag) {
+        // set the default tag if tag is empty
+        req.setTag(TextUtils.isEmpty(tag) ? TAG : tag);
+        getRequestQueue().add(req);
+    }
+
+    public <T> void addToRequestQueue(Request<T> req) {
+        req.setTag(TAG);
+        getRequestQueue().add(req);
+    }
+
+    public void cancelPendingRequests(Object tag) {
+        if (mRequestQueue != null) {
+            mRequestQueue.cancelAll(tag);
+        }
+    }
+}
